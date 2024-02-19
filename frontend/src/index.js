0d29dd22e41d4a439af8d34baeda1a7a8baabf7d
Makhafagy <makhafagy11@gmail.com>
2024-02-19 15:13:05 -0800

    Simple user login frontend created

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,17 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './components/App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './components/App'
+import LoginForm from './views/user/login/UserLogin'
+import reportWebVitals from './reportWebVitals'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+    <div>
+      <App /> {/* Render the App component */}
+      <LoginForm /> {/* Render the LoginForm component */}
+    </div>
+  </React.StrictMode>,
+  document.getElementById('root')
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals()
