bed6c4d77e3ce57548ce280b40312c0b43bb755d
Makhafagy <makhafagy11@gmail.com>
2024-02-25 02:05:42 -0800

    Appheader connected to other components to show title and tabs to reduce code

--- frontend/src/views/header/AppHeader.js
+++ frontend/src/views/header/AppHeader.js
@@ -1,27 +1,28 @@
 // InitialPage.js
-import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import Tabs from '../../components/tab/Tabs';
-import '../../components/Index.css';
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import Tabs from '../../components/tab/Tabs'
+import '../../components/Index.css'
 
 const AppHeader = () => {
-  const [activeTab, setActiveTab] = useState('Home');
-  const navigate = useNavigate();
+  const [activeTab, setActiveTab] = useState('Home')
+  const navigate = useNavigate()
 
   const handleTabChange = tab => {
-    setActiveTab(tab);
-    const path = tabsPaths[tabLabels.indexOf(tab)];
-    navigate(path);
-  };
+    setActiveTab(tab)
+    const path = tabsPaths[tabLabels.indexOf(tab)]
+    navigate(path)
+  }
 
-  const tabLabels = ['Home', 'Profile'];
-  const tabsPaths = ['/home', '/user/profile'];
+  const tabLabels = ['Home', 'Profile']
+  const tabsPaths = ['/home', '/user/profile']
 
   return (
     <div>
+      <h1 className='title'>Career Clique</h1>
       <Tabs activeTab={activeTab} onTabChange={handleTabChange} tabLabels={tabLabels} />
     </div>
-  );
-};
+  )
+}
 
-export default AppHeader;
+export default AppHeader
