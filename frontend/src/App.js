50549cdd579fb4aacb22a34b2debbb50aa9a79c6
Makhafagy <makhafagy11@gmail.com>
2024-02-24 22:13:18 -0800

    User authentication fully in place, passwords hashed, implemented authentication using JWT as the middleware, Multiple styling improvements, multiple server side improvements, code readability significantly improved, profile is now shown with accurate user information upon login success, logout also is in place to clear and log user out

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,12 @@
-import React, { useState } from 'react'
-import Homepage from './views/home/Homepage'
+// App.js
+import React from 'react'
+import AppHeader from './views/header/AppHeader'
 
 const App = () => {
-  const [activeTab, setActiveTab] = useState('home')
-
-  const handleTabChange = tab => {
-    setActiveTab(tab)
-  }
-
   return (
     <div className='main-container'>
       <h1 className='title'>Career Clique</h1>
-      <Homepage activeTab={activeTab} onTabChange={handleTabChange} />
+      <AppHeader />
     </div>
   )
 }
---
+++ frontend/src/views/user/UserProfile.js
@@ -0,0 +1,15 @@
+// // UserProfile.js
+// import React, { useState } from 'react'
+// import Profiles from '../../components/profile/Profiles'
+
+// const UserProfile = () => {
+//   const [isLoggedIn, setIsLoggedIn] = useState(false)
+
+//   return (
+//     <div>
+//       <Profiles isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} />
+//     </div>
+//   )
+// }
+
+// export default UserProfile
